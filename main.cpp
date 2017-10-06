b7c0253223fd294a1099722f620c6108778c441d
Mark Mahoney <markm208@gmail.com>
2017-10-06 13:01:28 -0500

    Added power

--- main.cpp
+++ main.cpp
@@ -2,14 +2,24 @@
 
 using namespace std;
 
-void square(int num);
+void power(int base, int exponent);
 
 int main()
 {
-    return 0;
+    power(2, 3);
+    power(2, 10);
+    power(2, 20);
+
+	return 0;
 }
 //--
-void square(int num)
+void power(int base, int exponent)
 {
-    cout<<(num * num)<<endl;
+    int retVal = 1;
+
+    for(int i = 0;i < exponent;i++)
+    {
+        retVal = retVal * base;
+    }
+    cout<<retVal<<endl;
 }
